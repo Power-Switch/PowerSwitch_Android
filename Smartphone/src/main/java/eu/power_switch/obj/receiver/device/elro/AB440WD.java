9ab199b4791f43a3ecfbfbd16fe4ff69b2121f27
Markus Ressel <markus9100@gmail.com>
2016-03-07 13:49:30 +0100

    fixed wrong elro inheritance

--- Smartphone/src/main/java/eu/power_switch/obj/receiver/device/elro/AB440WD.java
+++ Smartphone/src/main/java/eu/power_switch/obj/receiver/device/elro/AB440WD.java
@@ -21,22 +21,42 @@ package eu.power_switch.obj.receiver.device.elro;
 import android.content.Context;
 
 import java.util.LinkedList;
+import java.util.List;
 
+import eu.power_switch.R;
+import eu.power_switch.obj.button.Button;
+import eu.power_switch.obj.button.OffButton;
+import eu.power_switch.obj.button.OnButton;
+import eu.power_switch.obj.gateway.BrematicGWY433;
+import eu.power_switch.obj.gateway.ConnAir;
 import eu.power_switch.obj.gateway.Gateway;
+import eu.power_switch.obj.gateway.ITGW433;
 import eu.power_switch.obj.receiver.DipReceiver;
 import eu.power_switch.obj.receiver.DipSwitch;
 import eu.power_switch.obj.receiver.Receiver;
 import eu.power_switch.shared.exception.gateway.GatewayNotSupportedException;
 import eu.power_switch.shared.exception.receiver.ActionNotSupportedException;
 
-public class AB440WD extends AB440ID implements DipReceiver {
+public class AB440WD extends Receiver implements DipReceiver {
     // dips are 12345 ABCDE
-    // identical to "AB440ID" (but more dips)
 
-    private static final String MODEL = Receiver.getModelName(AB440WD.class.getCanonicalName());
+    static Brand BRAND = Brand.ELRO;
+    static String MODEL = Receiver.getModelName(AB440WD.class.getCanonicalName());
+
+    LinkedList<DipSwitch> dipList;
+
+    private String tx433version = "1,";
+
+    private String sSpeedConnAir = "14";
+    private String headConnAir = "TXP:0,0,10,5600,350,25,";
+    private String tailConnAir = tx433version + sSpeedConnAir + ";";
+
+    private String sSpeedITGW = "125,";
+    private String headITGW = "0,0,10,11200,350,26,0,";
+    private String tailITGW = tx433version + sSpeedITGW + "0";
 
     public AB440WD(Context context, Long id, String name, LinkedList<Boolean> dips, Long roomId) {
-        super(context, id, name, dips, roomId);
+        super(context, id, name, BRAND, MODEL, Type.DIPS, roomId);
         dipList = new LinkedList<>();
 
         if (dips != null && dips.size() == 10) {
@@ -63,11 +83,83 @@ public class AB440WD extends AB440ID implements DipReceiver {
             dipList.add(new DipSwitch("E", false));
         }
 
-        model = MODEL;
+        buttons.add(new OnButton(context, id));
+        buttons.add(new OffButton(context, id));
+    }
+
+    public List<String> getDipNames() {
+        List<String> d = new LinkedList<>();
+        for (DipSwitch dipSwitch : dipList) {
+            d.add(dipSwitch.getName());
+        }
+        return d;
     }
 
     @Override
     protected String getSignal(Gateway gateway, String action) throws GatewayNotSupportedException, ActionNotSupportedException {
-        return super.getSignal(gateway, action);
+        boolean actionSupported = false;
+        for (Button button : buttons) {
+            if (button.getName().equals(action)) {
+                actionSupported = true;
+                break;
+            }
+        }
+        if (!actionSupported) {
+            throw new ActionNotSupportedException();
+        }
+
+        String seq = "";
+        String lo = "1,";
+        String hi = "3,";
+
+        // segments of four
+        String seqLo = lo + hi + lo + hi; // low
+        @SuppressWarnings("unused")
+        String seqHi = hi + lo + hi + lo; // high (never used)
+        String seqFl = lo + hi + hi + lo; // floating
+
+        String on = seqLo + seqFl;
+        String off = seqFl + seqLo;
+
+        for (DipSwitch dip : dipList) {
+            if (dip.isChecked()) {
+                seq += seqLo;
+            } else {
+                seq += seqFl;
+            }
+        }
+
+        if (gateway.getClass() == ConnAir.class) {
+            if (action.equals(context.getString(R.string.on))) {
+                String ON = headConnAir + seq + on + tailConnAir;
+                return ON;
+            } else {
+                String OFF = headConnAir + seq + off + tailConnAir;
+                return OFF;
+            }
+        } else if (gateway.getClass() == BrematicGWY433.class) {
+            if (action.equals(context.getString(R.string.on))) {
+                String ON = headConnAir + seq + on + tailConnAir;
+                return ON;
+            } else {
+                String OFF = headConnAir + seq + off + tailConnAir;
+                return OFF;
+            }
+        } else if (gateway.getClass() == ITGW433.class) {
+            if (action.equals(context.getString(R.string.on))) {
+                String ON = headITGW + seq + on + tailITGW;
+                return ON;
+            } else {
+                String OFF = headITGW + seq + off + tailITGW;
+                return OFF;
+            }
+        } else {
+            throw new GatewayNotSupportedException();
+        }
+    }
+
+    @Override
+    public LinkedList<DipSwitch> getDips() {
+        return dipList;
     }
 }
